877e532bc16bdcf13313deeebd880f96824255d6
quannh997 <61110829+quannh997@users.noreply.github.com>
2020-05-17 22:17:47 +0700

    git push
    git push

---
+++ 1005 Java/js/index.js
@@ -0,0 +1,8 @@
+// console.log("Hello")
+var helloWorld = "Hello world"
+var number = 10
+console.log(helloWorld,typeof(HelloWorld) )
+console.log(number,typeof(number))
+// 3 cách khai báo biến
+let helloWorld2 = "hiih"
+const helloWorld3 = "haha"
